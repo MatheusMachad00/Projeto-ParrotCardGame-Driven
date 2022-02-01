f564acb1b826a24503e68695599a037848619cf3
MatheusMachad00 <matheus_m_machado@hotmail.com>
2022-02-01 15:41:21 -0300

    layout quase completo e criação do js

---
+++ script.js
@@ -0,0 +1,7 @@
+/*function cardNumber(){
+    let x = prompt("Digite o número de cartas (4 a 14):")
+    while (x >= 14 || x <= 4 && x % 2 === 1){
+        return prompt("Digite o número de cartas (4 a 14):");
+    }
+}*/
+
