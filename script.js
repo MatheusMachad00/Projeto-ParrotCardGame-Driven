77de5d2578ffc29838ffc7a136e294628ffda191
MatheusMachad00 <matheus_m_machado@hotmail.com>
2022-02-02 15:45:40 -0300

    Criação da função que pede a quantidade de cartas

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-/*function cardNumber(){
-    let x = prompt("Digite o número de cartas (4 a 14):")
-    while (x >= 14 || x <= 4 && x % 2 === 1){
-        return prompt("Digite o número de cartas (4 a 14):");
-    }
-}*/
+let numCard = 0;
 
+/* Função para quantidade de cartas*/
+function cardQtd(){
+    let i = parseInt(prompt("Digite o número de cartas (4 a 14):"));
+    while(i > 14 || i < 4 || i % 2 !== 0){
+        i = parseInt(prompt("Número inválido. Digite o número de cartas (4 a 14):"));
+    }
+    numCard = i;
+}
+
+/* Função  */
