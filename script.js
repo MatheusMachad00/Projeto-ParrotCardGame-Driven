c784986c47bcddf4d33ebc3e54f5d7151b614a2b
MatheusMachad00 <matheus_m_machado@hotmail.com>
2022-02-05 14:44:49 -0300

    Criada função virar carta e função randomizar. Adicionada animações ao virar a carta

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let numCard = 0;
+let parrots = ['imagens/bobrossparrot.gif', 'imagens/explodyparrot.gif', 'imagens/fiestaparrot.gif', 'imagens/metalparrot.gif', 'imagens/revertitparrot.gif', 'imagens/tripletsparrot.gif', 'imagens/unicornparrot.gif']
 
 /* Função para quantidade de cartas*/
 function cardQtd(){
@@ -9,4 +10,25 @@ function cardQtd(){
     numCard = i;
 }
 
-/* Função  */
+/* Função para randomizar as cartas */
+function randomizer() { 
+	return Math.random() - 0.5; 
+}
+
+/* Função para criar o jogo (FUNÇÃO INCOMPLETA!!!!!!!!)*/
+/*function createGame(numCard){
+    const ul = document.querySelector("ul");
+    for(let i = 0; i < numCard / 2; i++ ){
+        ul.innerHTML += `<div class="card">
+        <img src="imagens/front.png" alt="front_card">
+    </div>`
+    }
+}*/
+
+
+/* Função para virar a carta */
+const cards = document.querySelectorAll('.card');
+function flipCard() {
+    this.classList.toggle('flip');
+}
+cards.forEach(card => card.addEventListener('click', flipCard));
