436202f055e61ba9353559099848c28c2f013355
MatheusMachad00 <matheus_m_machado@hotmail.com>
2022-02-07 11:36:53 -0300

    Implementação da função que cria o jogo

--- script.js
+++ script.js
@@ -1,33 +1,41 @@
 let numCard = 0;
-let parrots = ['imagens/bobrossparrot.gif', 'imagens/explodyparrot.gif', 'imagens/fiestaparrot.gif', 'imagens/metalparrot.gif', 'imagens/revertitparrot.gif', 'imagens/tripletsparrot.gif', 'imagens/unicornparrot.gif'];
+let parrots = ['bobrossparrot', 'explodyparrot', 'fiestaparrot', 'metalparrot', 'revertitparrot', 'tripletsparrot', 'unicornparrot'];
 const allCards = document.querySelectorAll('.card');
 let hasFlippedCard = false;
 let firstCard, secondCard;
 let lockBoard = false;
+let board =[];
 
 /* Função para quantidade de cartas*/
-function cardQtd() {
+function gameStart() {
     let i = parseInt(prompt("Digite o número de cartas (4 a 14):"));
     while (i > 14 || i < 4 || i % 2 !== 0) {
         i = parseInt(prompt("Número inválido. Digite o número de cartas (4 a 14):"));
     }
     numCard = i;
+    for (let j = 0; j < numCard / 2; j++) {
+        board.push(allCards[j]);
+        board.push(allCards[j]);
+    }
+
+    board.sort(randomizer);
+    const main = document.querySelector("main");
+    main.innerHTML = "";
+
+    for (let i = 0; i < cardsQuantity; i++) {
+        main.innerHTML += `
+        <div class="card" data-framework="${board[j]}">
+        <img class="front-face" src="imagens/front.png" alt="front">
+        <img class="back-face" src="imagens/${board[j]}.gif" alt="${board[j]}">
+    </div>
+        `;
+    }    
 }
 
 /* Função para randomizar as cartas */
 function randomizer() {
     return Math.random() - 0.5;
 }
-
-/* Função para criar o jogo (FUNÇÃO INCOMPLETA!!!!!!!!)*/
-/*function createGame(numCard){
-    const ul = document.querySelector("ul");
-    for(let i = 0; i < numCard / 2; i++ ){
-        ul.innerHTML += `<div class="card">
-        <img src="imagens/front.png" alt="front_card">
-    </div>`
-    }
-}*/
 
 /* Função para virar a carta*/
 function flipCard() {
